2edfa3f30687668fd27e79f8d2c8b16d0052753e
jungyuno <soda9894@gmail.com>
2025-01-24 12:55:46 +0900

    Test

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,74 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react';
+// import './App.css'
+
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [country, setCountry] = useState([]);
+  const [countryName, setCountryName] = useState("");
+  const [gold, setGold] = useState(0);
+  const [silver, setSilver] = useState(0);
+  const [bronze, setBronze] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>파리 올림픽</h1>
+      <form>
+        <label>
+          국가명
+          <input type="text" placeholder="국가 입력" />
+        </label>
+
+        <label>
+          금메달
+          <input
+            type="number"
+            value={gold}
+            onChange={(e) => {
+              setGold(e.target.value);
+            }}
+          />
+        </label>
+
+        <label>
+          은메달
+          <input
+            type="number"
+            value={silver}
+            onChange={(e) => {
+              setSilver(e.target.value);
+            }}
+          />
+        </label>
+
+        <label>
+          동메달
+          <input
+            type="number"
+            value={bronze}
+            onChange={(e) => {
+              setBronze(e.target.value);
+            }}
+          />
+        </label>
+
+        <button type="button">국가 추가</button>
+        <button type="button">업데이트</button>
+      </form>
+
+      <testComponent>
+        <div>테스트 올림픽</div>
+      </testComponent>
+    </div>
+  );
 }
 
-export default App
+function testComponent({ children }) {
+  return (
+    <div>
+      <div color="red">{children}</div>
+    </div>
+  );
+}
+
+export default App;
